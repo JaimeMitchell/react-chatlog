b9d7b0903b6500483b374d0ca218c35f2c3a4307
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-12-16 14:01:34 -0500

    Finished Wave 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
+// const chatMessage1 = chatMessages[0]["body"];
 const App = () => {
   return (
     <div id="App">
@@ -9,11 +11,19 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+  
+        <ChatEntry
+        // id = {chatMessages.id}
+        sender = {chatMessages[0].sender}
+        body = {chatMessages[0].body}
+        timeStamp = {chatMessages[0].timeStamp}
+        // liked = {chatMessages.liked}
+        />
       </main>
     </div>
   );
 };
 
 export default App;
+/* Wave 01: Render one ChatEntry component
+  Wave 02: Render ChatLog component */
